048235ed5376bc35d6e6ab6a4342811e0c8f09c8
467441053 <467441053@qq.com>
2019-12-07 14:13:55 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=467441053@qq.com
 
 export MYCLUSTER=tektonknativebeijing05
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing05/kube-config-tok04-tektonknativebeijing05.yml
 
 export NAMESPACE=jiangyou
 export EMAIL=467441053@qq.com
